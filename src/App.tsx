d485be8ce9eba875b3d164c4fa1180c245f8f5be
petsamuel <petsamuel4@gmail.com>
2025-01-15 13:37:59 +0100

    🎉 initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="m-4">
+      <div className="grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 lg:grid-rows-12 gap-2 h-[150svh] md:h-svh">
+        {/* cols-1 */}
+        <div className="lg:col-span-1   lg:row-span-6 md:row-span-2  place-content-center items-center ">
+          <img
+            src={"https://photo-grid-xi.vercel.app/assets/sea-B7vGIZvm.webp"}
+            alt={"img-1"}
+            className="object-cover h-full  rounded-2xl  w-full"
+          />
+        </div>
+        <div className="lg:col-span-3 lg:row-span-6 md:row-span-3  place-content-center items-center  ">
+          <img
+            src={
+              "https://photo-grid-xi.vercel.app/assets/chun-li-BpwPOov9.webp"
+            }
+            alt={"img-2"}
+            className="object-cover h-full  rounded-2xl w-full"
+          />
+        </div>
+        <div className="lg:col-span-1 lg:row-span-6  md:row-span-2   place-content-center items-center ">
+          <img
+            src={"https://photo-grid-xi.vercel.app/assets/var-DRAoKdZB.webp"}
+            alt={"3"}
+            className="object-cover  h-full  rounded-2xl w-full"
+          />
+        </div>
+        <div className="lg:col-span-1  lg:row-span-3 md:row-span-2  place-content-center items-center ">
+          <img
+            src={
+              "https://photo-grid-xi.vercel.app/assets/restaurant-DDiWP-jG.webp"
+            }
+            alt={"4"}
+            className="object-cover h-full  rounded-2xl w-full"
+          />
+        </div>
+        <div className="lg:col-span-2 lg:row-span-6  md:row-span-4   place-content-center items-center ">
+          <img
+            src={
+              "https://photo-grid-xi.vercel.app/assets/astronaut-gOCFgbpq.webp"
+            }
+            alt={"5"}
+            className="object-center h-full  rounded-2xl w-full"
+          />
+        </div>
+        <div className="lg:col-span-2 lg:row-span-3 md:row-span-2  place-content-center items-center ">
+          <img
+            src={
+              "https://photo-grid-xi.vercel.app/assets/bedroom-DTV6Z5Pv.webp"
+            }
+            alt={"6"}
+            className="object-cover h-full  rounded-2xl w-full"
+          />
+        </div>
+        <div className="lg:col-span-1  lg:row-span-3 md:row-span-3  place-content-center items-center ">
+          <img
+            src={
+              "https://photo-grid-xi.vercel.app/assets/var-baddie-D6LsFdTV.webp"
+            }
+            alt={"7"}
+            className="object-center h-full  rounded-2xl w-full"
+          />
+        </div>
+        <div className="lg:col-start-4 lg:col-end-6 lg:row-span-3  md:row-span-2 place-content-center items-center ">
+          <img
+            src={
+              "https://photo-grid-xi.vercel.app/assets/dark-mannequin-CKI6McgY.webp"
+            }
+            alt={"8"}
+            className="object-center h-full  rounded-2xl w-full"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
