9d15d09e8e93b5c66ce77628133d7da9a7897865
petsamuel <petsamuel4@gmail.com>
2025-01-16 16:25:09 +0100

    Enable landing page display and adjust layout for improved responsiveness

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -10,7 +10,7 @@ import img8 from "../assets/images/illustration-schedule-posts.webp";
 export const LandingPage = () => {
   return (
     <div className="m-4 font-DmSans bg-white">
-      <div className="grid lg:grid-cols-4 lg:grid-rows-2 auto-rows-auto lg:h-[180svh]  lg:gap-10 gap-5  overflow-hidden relative w-full w-dvh">
+      <div className="grid lg:grid-cols-4 lg:grid-rows-2 auto-rows-auto lg:h-[200svh]  lg:gap-10 gap-5  overflow-hidden relative w-full">
         <div className="bg-yellows-100 lg:col-span-1 lg:row-auto rounded-xl flex items-center lg:h-[33rem]   ">
           <div className="p-6 flex flex-col justify-center items-center gap-8">
             <p className="font-medium text-5xl">
@@ -42,8 +42,8 @@ export const LandingPage = () => {
           </div>
         </div>
         <div className="bg-purple-100 lg:col-span-1 lg:row-span-2 rounded-xl overflow-hidden row-start-4">
-          <div className="p-6 flex flex-col lg:justify-between justify-center gap-4 items-center  w-full">
-            <p className="font-medium text-4xl text-balance my-4">
+          <div className="p-6 flex flex-col lg:justify-evenly justify-center gap-4 items-center  w-full h-full">
+            <p className="font-medium text-4xl text-balance mt-4">
               Schedule to social media.
             </p>
             <img
@@ -70,7 +70,7 @@ export const LandingPage = () => {
           </div>
         </div>
         <div className="bg-yellows-500  rounded-xl shadow-lg overflow-hidden lg:row-auto row-start-3">
-          <div className="p-6 flex flex-col justify-center lg:items-center items-start gap-8  my-4">
+          <div className="p-6 flex flex-col justify-center lg:items-center items-start gap-8  my-4 h-full">
             <p className="lg:text-4xl text-5xl lg:mb-4 mb-10 font-medium ">
               {" "}
               Maintain a consistent posting schedule.
@@ -78,14 +78,17 @@ export const LandingPage = () => {
             <img src={img3} alt="" className="object-cover w-[30rem] " />
           </div>
         </div>
-        <div className="bg-yellows-500 lg:col-span-1  rounded-xl overflow-hidden lg:row-start-2 lg:row-end-4  lg:mt-[10rem]">
-          <div className="p-6 flex flex-col justify-center items-center gap-8 font-medium text-5xl text-balance">
+        <div className="bg-yellows-500 lg:col-span-1  rounded-xl overflow-hidden lg:row-start-2 lg:mt-[8rem] lg:row-span-2">
+          <div className="p-6 flex flex-col justify-center lg:justify-between items-center gap-8 font-medium text-5xl text-balance h-full">
             <p>Write your content using AI.</p>
-            <img src={img1} className="object-cover  max-w-[16rem]" />
+            <div>
+              {" "}
+              <img src={img1} className="object-cover  " />
+            </div>
           </div>
         </div>
-        <div className="bg-white shadow-xl rounded-xl shadom-sm py-4 row-start-6">
-          <div className="p-6 flex flex-col  gap-4 ">
+        <div className="bg-white shadow-xl rounded-xl shadom-sm py-4 row-start-6 lg:col-start-2 lg:row-auto ">
+          <div className="p-6 flex flex-col  gap-4 h-full">
             <div className="flex flex-col font-medium text-7xl text-balance gap-4">
               <h1 className="">{">"}56% </h1>
               <h3 className="text-2xl font-medium">faster audience growth</h3>
@@ -97,8 +100,8 @@ export const LandingPage = () => {
             />
           </div>
         </div>
-        <div className="bg-purples-500 lg:col-span-2 rounded-xl row-start-5">
-          <div className="p-6 flex lg:flex-row flex-col items-center gap-4 text-white text-4xl font-medium ">
+        <div className="bg-purples-500 lg:col-start-3 lg:col-end-5 rounded-xl row-start-5 lg:row-auto">
+          <div className="p-6 flex lg:flex-row flex-col items-center gap-4 text-white text-4xl font-medium h-full">
             <img src={img6} alt="" className="object-contain w-[18rem] " />
             <p>Grow followers with non-stop content.</p>
           </div>
