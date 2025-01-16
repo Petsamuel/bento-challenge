53c4273b42116352a1bfa1e8ccce9216d4247e16
petsamuel <petsamuel4@gmail.com>
2025-01-16 15:32:56 +0100

    Refactor LandingPage component for improved layout and styling consistency

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -9,8 +9,8 @@ import img8 from "../assets/images/illustration-schedule-posts.webp";
 
 export const LandingPage = () => {
   return (
-    <div className="lg:m-4 font-DmSans bg-white">
-      <div className="grid lg:grid-cols-4 md:grid-cols-2  lg:grid-rows-2 auto-rows-auto lg:h-[180svh]  lg:gap-10 gap-5  lg:overflow-hidden relative w-full w-dvh">
+    <div className="m-4 font-DmSans bg-white">
+      <div className="grid lg:grid-cols-4 lg:grid-rows-2 auto-rows-auto lg:h-[180svh]  lg:gap-10 gap-5  overflow-hidden relative w-full w-dvh">
         <div className="bg-yellows-100 lg:col-span-1 lg:row-auto rounded-xl flex items-center lg:h-[33rem]   ">
           <div className="p-6 flex flex-col justify-center items-center gap-8">
             <p className="font-medium text-5xl">
@@ -24,9 +24,9 @@ export const LandingPage = () => {
             />
           </div>
         </div>
-        <div className="bg-purples-500 lg:col-span-2 rounded-xl lg:row-auto flex items-center lg:overflow-hidden   row-start-1">
+        <div className="bg-purples-500 lg:col-span-2 rounded-xl lg:row-auto flex items-center overflow-hidden   row-start-1">
           <div className="p-6 flex flex-col justify-center items-center place-content-center  gap-4 text-white">
-            <h1 className="text-7xl font-medium  text-center">
+            <h1 className="text-7xl font-medium  text-center text-balance">
               {" "}
               Social Media <span className="text-yellows-500">10x</span> Faster
               with AI{" "}
@@ -41,23 +41,23 @@ export const LandingPage = () => {
             </div>
           </div>
         </div>
-        <div className="bg-purple-100 lg:col-span-1 lg:row-span-2 rounded-xl lg:overflow-hidden row-start-4">
-          <div className="p-6 flex flex-col justify-between gap-4 items-center  w-full">
+        <div className="bg-purple-100 lg:col-span-1 lg:row-span-2 rounded-xl overflow-hidden row-start-4">
+          <div className="p-6 flex flex-col lg:justify-between justify-center gap-4 items-center  w-full">
             <p className="font-medium text-4xl text-balance my-4">
               Schedule to social media.
             </p>
             <img
               src={img8}
               alt="img-6"
-              className="object-cover flex justify-end  ml-[18rem] min-w-[30rem] "
+              className="object-cover flex justify-end  lg:ml-[18rem] lg:min-w-[30rem] "
             />
-            <p className="font-normal pt-[1rem] text-balance text-xl w-full">
+            <p className="font-normal pt-[1rem] text-balance  w-full lg:text-xl text-2xl text-center lg:text-start">
               Optimize post timings to publish content at the perfect time for
               your audience.
             </p>
           </div>
         </div>
-        <div className="bg-white shadow-xl  rounded-xl lg:overflow-hidden lg:col-start-2 lg:col-end-2 row-start-2">
+        <div className="bg-white shadow-xl  rounded-xl overflow-hidden lg:col-start-2 lg:col-end-2 row-start-2">
           <div className="p-5 flex flex-col justify-center items-center gap-4 my-4 w-full">
             <img
               src={img7}
@@ -69,16 +69,16 @@ export const LandingPage = () => {
             </p>
           </div>
         </div>
-        <div className="bg-yellows-500  rounded-xl shadow-lg lg:overflow-hidden lg:row-auto row-start-3">
-          <div className="p-6 flex flex-col justify-center items-center gap-8  my-4">
-            <p className="text-4xl mb-4 font-medium ">
+        <div className="bg-yellows-500  rounded-xl shadow-lg overflow-hidden lg:row-auto row-start-3">
+          <div className="p-6 flex flex-col justify-center lg:items-center items-start gap-8  my-4">
+            <p className="lg:text-4xl text-5xl lg:mb-4 mb-10 font-medium ">
               {" "}
               Maintain a consistent posting schedule.
             </p>
             <img src={img3} alt="" className="object-cover w-[30rem] " />
           </div>
         </div>
-        <div className="bg-yellows-500 lg:col-span-1  rounded-xl lg:overflow-hidden lg:row-start-2 lg:row-end-4  lg:mt-[10rem]">
+        <div className="bg-yellows-500 lg:col-span-1  rounded-xl overflow-hidden lg:row-start-2 lg:row-end-4  lg:mt-[10rem]">
           <div className="p-6 flex flex-col justify-center items-center gap-8 font-medium text-5xl text-balance">
             <p>Write your content using AI.</p>
             <img src={img1} className="object-cover  max-w-[16rem]" />
@@ -88,17 +88,17 @@ export const LandingPage = () => {
           <div className="p-6 flex flex-col  gap-4 ">
             <div className="flex flex-col font-medium text-7xl text-balance gap-4">
               <h1 className="">{">"}56% </h1>
-              <h3 className="text-xl">faster audience growth</h3>
+              <h3 className="text-2xl font-medium">faster audience growth</h3>
             </div>
             <img
               src={img2}
               alt=""
-              className="object-cover flex justify-end w-[12rem] pt-4"
+              className="object-cover flex justify-end w-[18rem] pt-4"
             />
           </div>
         </div>
         <div className="bg-purples-500 lg:col-span-2 rounded-xl row-start-5">
-          <div className="p-6 flex items-center gap-4 text-white text-4xl font-medium ">
+          <div className="p-6 flex lg:flex-row flex-col items-center gap-4 text-white text-4xl font-medium ">
             <img src={img6} alt="" className="object-contain w-[18rem] " />
             <p>Grow followers with non-stop content.</p>
           </div>
